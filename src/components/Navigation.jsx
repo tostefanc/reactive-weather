1cf20ff33036d703620695d05c05cd94df1429ea
tostefanc <stefan.toma100@gmail.com>
2021-05-04 21:12:01 +0300

    responsive navbar, react-bootstrap

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,30 +1,55 @@
 import {Link} from "react-router-dom";
 import Search from "./search";
 import React from "react";
-
+import {Navbar, Nav, NavDropdown, Form, Button, FormControl} from "react-bootstrap";
 
 
 export default function Navigation() {
 return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
+    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    //     <div className="collapse navbar-collapse">
+    //
+    //         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //             <ul className="navbar-nav mr-auto">
+    //                <li className="nav-item">
+    //                     <Link className="nav-link" to={"/"}>Home</Link>
+    //                </li>
+    //                 <li className="nav-item">
+    //                      <Link className="nav-link" to={"/city/bucharest,ro"}>Bucharest</Link>
+    //                  </li>
+    //                 <li className="nav-item">
+    //                      <Link className="nav-link" to={"/city/warsaw,pl"}>Warsaw</Link>
+    //                  </li>
+    //                  <li className="nav-item">
+    //                     <Link className="nav-link" to={"/city/budapest,hu"}>Budapest</Link>
+    //                  </li>
+    //             </ul>
+    //             <Search></Search>
+    //          </div>
+    //     </div>
+    //  </nav>
+
+
+    <Navbar bg="dark" expand="lg" variant="dark">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+                <Nav.Link>
                     <Link className="nav-link" to={"/"}>Home</Link>
-                </li>
-                <li className="nav-item">
+                </Nav.Link>
+                <Nav.Link>
                     <Link className="nav-link" to={"/city/bucharest,ro"}>Bucharest</Link>
-                </li>
-                <li className="nav-item">
+                </Nav.Link>
+                <Nav.Link>
                     <Link className="nav-link" to={"/city/warsaw,pl"}>Warsaw</Link>
-                </li>
-                <li className="nav-item">
+                </Nav.Link>
+                <Nav.Link>
                     <Link className="nav-link" to={"/city/budapest,hu"}>Budapest</Link>
-                </li>
-            </ul>
+                </Nav.Link>
+            </Nav>
             <Search></Search>
-        </div>
-    </nav>
+        </Navbar.Collapse>
+    </Navbar>
 )
 }
 
