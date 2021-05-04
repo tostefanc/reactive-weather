a5b32caf410d65962fecaea1c29d943477cdbf70
tostefanc <stefan.toma100@gmail.com>
2021-05-04 21:12:59 +0300

    housekeeping responsive

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,34 +1,11 @@
 import {Link} from "react-router-dom";
 import Search from "./search";
 import React from "react";
-import {Navbar, Nav, NavDropdown, Form, Button, FormControl} from "react-bootstrap";
+import {Navbar, Nav} from "react-bootstrap";
 
 
 export default function Navigation() {
 return (
-    // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    //     <div className="collapse navbar-collapse">
-    //
-    //         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //             <ul className="navbar-nav mr-auto">
-    //                <li className="nav-item">
-    //                     <Link className="nav-link" to={"/"}>Home</Link>
-    //                </li>
-    //                 <li className="nav-item">
-    //                      <Link className="nav-link" to={"/city/bucharest,ro"}>Bucharest</Link>
-    //                  </li>
-    //                 <li className="nav-item">
-    //                      <Link className="nav-link" to={"/city/warsaw,pl"}>Warsaw</Link>
-    //                  </li>
-    //                  <li className="nav-item">
-    //                     <Link className="nav-link" to={"/city/budapest,hu"}>Budapest</Link>
-    //                  </li>
-    //             </ul>
-    //             <Search></Search>
-    //          </div>
-    //     </div>
-    //  </nav>
-
 
     <Navbar bg="dark" expand="lg" variant="dark">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
